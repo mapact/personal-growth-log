0e07fc3fc17d4a869e0381dd1e283a13dbed9e01
Li Shing Chan <lischan@microsoft.com>
2020-06-29 00:07:04 +0800

    still trying the graph

--- growth_log/public/app.js
+++ growth_log/public/app.js
@@ -4,15 +4,15 @@ $(()=>{
     console.log('connected with graph.ejs') //this wasnt printed
 
     //declare graph variables (date and rate)
-    const rateGraph = []; //nothing render cuz i dont know how to pass data here
-    console.log(dateGraph)
+    const rateGraph = [(logs.rate)]; //nothing render cuz i dont know how to pass data here
+    console.log(rateGraph)
 
-    const ctx = $('#myChart')
-    let timeGraph = new Chart(ctx, {
-        type: 'line',
-        data: [{
-            x: new Date(),
-            y: 1
-        }]
-    })
+    // const ctx = $('#myChart')
+    // let timeGraph = new Chart(ctx, {
+    //     type: 'line',
+    //     data: [{
+    //         x: $('#date').val() = ,
+    //         y: $('#rate')
+    //     }]
+    // })
 });

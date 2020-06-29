a39a571c0fc0ab9bf9640edd2257516d90961144
Li Shing Chan <lischan@microsoft.com>
2020-06-29 22:20:59 +0800

    make graph - first step

--- growth_log/public/app.js
+++ growth_log/public/app.js
@@ -1,18 +1,36 @@
 
 
 $(()=>{
-    console.log('connected with graph.ejs') //this wasnt printed
+    console.log('connected with graph.ejs'); //this wasnt printed
 
     //declare graph variables (date and rate)
-    const rateGraph = [(logs.rate)]; //nothing render cuz i dont know how to pass data here
-    console.log(rateGraph)
-
-    // const ctx = $('#myChart')
-    // let timeGraph = new Chart(ctx, {
-    //     type: 'line',
-    //     data: [{
-    //         x: $('#date').val() = ,
-    //         y: $('#rate')
-    //     }]
-    // })
+    let leftTable = $(".table > tbody > tr ")
+    //console.log(leftTable)
+    let dates = []
+    for (let i=0; i < leftTable.length; i++){
+        const date = leftTable[i].children[0].textContent
+        dates.push(date);
+    }
+    console.log(dates)
+    //console.log(leftTable[0].children[0].textContent)
+    // const date = $(dateList[0]).text();
+    // const rate = $(dateList[0]).text();
+    const ctx = $('#myChart')
+    const chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+        // The data for our dataset
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 1, 2, 3]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
 });
